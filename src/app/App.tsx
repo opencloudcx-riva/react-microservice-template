32504e9cdf87c34b5d245a3252504708892b6bf5
Adam Norris <anorris@rivasolutionsinc.com>
2022-02-28 21:23:23 +0000

    Set default message

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,7 @@ export const App: FunctionComponent = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Change this message to something interesting...
         </p>
         <a
           className="App-link"
