7d3ee9d70b6b62c0aac1e3405d296f8d795f6817
Adam Norris <anorris@rivasolutionsinc.com>
2022-03-28 13:19:27 +0000

    Changed message for demo test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,7 @@ export const App: FunctionComponent = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. Friday!!
+          Edit <code>src/App.tsx</code> and save to reload. Monday :(
         </p>
         <a
           className="App-link"
