601ad41105b346825b0b70d850427a9e50d1ccbe
Adam Norris <anorris@rivasolutionsinc.com>
2022-07-08 15:03:24 +0000

    Fixed end-tag issue

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,6 +9,7 @@ export const App: FunctionComponent = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
